949d29236157132e2768b169c968e3690b65d44d
volandese <105665179+volandese@users.noreply.github.com>
2022-07-13 12:28:10 -0500

    Add files via upload

---
+++ project-two/change.js
@@ -0,0 +1,19 @@
+function change02() {
+    document.getElementById("large").src = "images/grad-friends.jpeg";
+}
+
+function change03() {
+    document.getElementById("large").src = "images/nle-choppa.jpeg";
+}
+
+function change04() {
+    document.getElementById("large").src = "images/photography.jpeg";
+}
+
+function change05() {
+    document.getElementById("large").src = "images/six-flags.jpeg";
+}
+
+function change06() {
+    document.getElementById("large").src = "images/summerfest.jpeg";
+}
